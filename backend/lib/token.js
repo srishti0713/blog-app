d6d24717b0fce2f1049527bb8cd92f9f6630700c
Srishti Banerjee <srishtibanerjee7@gmail.com>
2026-02-16 20:28:40 +0530

    refactor secure to true

--- backend/lib/token.js
+++ backend/lib/token.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
 		maxAge: 7 * 24 * 60 * 60 * 1000,
 		httpOnly: true,
 		sameSite: "none",
-		secure: process.env.NODE_ENV === "production",
+		secure:true,
 	});
     return token;
 };
